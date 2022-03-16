b46a0a2a682e3b41c228008208cff5dcb4fb41bc
rosvali <rosval6578@gmail.com>
2022-03-16 18:27:59 +0100

    fix worker

--- p1/server.sh
+++ p1/server.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+
 echo "Installing net-tools..."
 yum -y install net-tools 
 
 echo "Installing k3s"
 systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.42.110
-  --bind-address=192.168.42.110
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server
+  --node-ip=192.168.42.110 
+  --bind-address=192.168.42.110 
   --advertise-address=192.168.42.110
-  --tls-san servertest
-  --write-kubeconfig-mode 644"  sh -
+  --write-kubeconfig-mode=644
+  --tls-san=192.168.42.110 
+  --node-external-ip=192.168.42.110" sh -
 
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
+mkdir -p /vagrant/token
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/token
 
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+echo "[Server complete]"
--- p1/server_worker.sh
+++ p1/server_worker.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 echo "Installing net-tools..."
 yum -y install net-tools
 
 echo "Installing k3s"
 systemctl disable firewalld --now
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--server https://192.168.42.112:6443
-  --token-file /vagrant/node-token
-  --node-ip=192.168.42.111"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent
+  --server https://192.168.42.110:6443
+  --token-file /vagrant/token/node-token
+  --node-ip=192.168.42.111" sh -
 
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
+
+echo "[Serverworker complete]"
