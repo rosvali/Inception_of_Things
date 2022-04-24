596ddf5aed8be4f6055530d7d3888eeb25d2ebf9
rosvali <rosval6578@gmail.com>
2022-04-24 12:36:19 +0200

    remove useless options

--- p1/server.sh
+++ p1/server.sh
@@ -7,11 +7,7 @@ echo "Installing k3s"
 systemctl disable firewalld --now
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server
   --node-ip=192.168.42.110 
-  --bind-address=192.168.42.110 
-  --advertise-address=192.168.42.110
-  --write-kubeconfig-mode=644
-  --tls-san=192.168.42.110 
-  --node-external-ip=192.168.42.110" sh -
+  --write-kubeconfig-mode=644" sh -
 
 mkdir -p /vagrant/token
 sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/token
