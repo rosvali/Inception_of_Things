cded05a9d405e129ea802ae36e3f819f1d968808
rosvali <rosval6578@gmail.com>
2022-05-03 14:14:16 +0200

    rename service

--- p2/server.sh
+++ p2/server.sh
@@ -8,6 +8,14 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.42.110
 
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
 
-# kubectl apply -f "/vagrant/app1_deployment.yaml"
-# kubectl apply -f "/vagrant/app1_service.yaml"
-# kubectl apply -f "/vagrant/app1_ingress.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app1/app1_deployment.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app1/app1_service.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app1/app1_ingress.yaml"
+
+/usr/local/bin/kubectl apply -f "/vagrant/app2/app2_deployment.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app2/app2_service.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app2/app2_ingress.yaml"
+
+/usr/local/bin/kubectl apply -f "/vagrant/app3/app3_deployment.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app3/app3_service.yaml"
+/usr/local/bin/kubectl apply -f "/vagrant/app3/app3_ingress.yaml"
