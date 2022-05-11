fe29f3b6017ca1bed92c082046f157e3534e3677
rosvali <rosval6578@gmail.com>
2022-05-11 13:06:10 +0200

    update script

--- p3/install.sh
+++ p3/install.sh
@@ -1,31 +1,39 @@
-# Create cluster
+echo "########## Creating cluster.. ##########"
 
-k3d cluster create iot-cluster --api-port 6443 -p 8080:80@loadbalancer
+k3d cluster create iot-cluster -p 8080:80@loadbalancer
 
-# Create namespaces
+echo "########## Creating namespaces.. ##########"
 
 kubectl create namespace argocd
 kubectl create namespace dev
 
-# Install argocd in argocd namespace
+echo "########## Installing argocd.. ##########"
 
 kubectl apply -n argocd -f argo.yaml
 
-# Waiting pod to be ready
+echo "########## Waiting pod to be ready.. ##########"
 
 kubectl -n argocd wait pod argocd-application-controller-0 --for=condition=Ready --timeout=-1s
 
-# Apply Argocd's ingress
+echo "########## Apply ingress.. ##########"
 
 kubectl apply -n argocd -f ingress.yaml
 
-# Get password for login Argocd CLI
+echo "########## Get password for argocd CLI.. ##########"
 
-# passwordsecret="kubectl get secret argocd-initial-admin-secret -n argocd -o yaml | grep "password: " | cut -b 13-"
-# echo $PASSWORDSECRET
-# $PASSWORD=$(echo $PASSWORD | base64 --decode)
-# ECHO $PASSWORD
+passwordsecret=$(kubectl get secret argocd-initial-admin-secret -n argocd -o yaml | grep "password: " | cut -b 13-)
+password=$(echo $passwordsecret | base64 --decode)
 
-# Login argocd CLI
+echo "########## Configure application.. ##########"
 
-# argocd login localhost --username admin --password $password
+kubectl apply -f application.yaml -n argocd
+kubectl apply -f project.yaml -n argocd
+
+echo "########## Installation completed.. ##########"
+
+echo "########## Argocd CLI: http://localhost:8080/argocd/ ##########"
+echo "########## Pseudo: admin ##########"
+echo "########## Password: $password ##########"
+echo "########## Application: http://localhost:8888/ ##########"
+
+kubectl port-forward -n dev svc/wil-playground-service 8888
