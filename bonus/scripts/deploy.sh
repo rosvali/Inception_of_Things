f22a8ad29e8ef5389dd068dbc7b50e0e4bfef7df
rosvali <rosval6578@gmail.com>
2022-06-14 20:08:02 +0200

    deploy gitlab with minikube minimun values

--- bonus/scripts/deploy.sh
+++ bonus/scripts/deploy.sh
@@ -1,14 +1,20 @@
-## Create gitlab namespace
-
+echo "create namespace for gitlab"
 kubectl create namespace gitlab
-
-## Deploy Gitlab with helm
 
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
-helm upgrade --install gitlab gitlab/gitlab \
+# git clone https://gitlab.com/gitlab-org/charts/gitlab.git
+cd gitlab
+
+# helm dependency update
+helm install gitlab . \
+  --namespace gitlab \
   --timeout 600s \
-  --set global.hosts.domain=localhost \
-  --set certmanager-issuer.email=me@example.com \
-  --namespace gitlab
+  -f /Users/rosvali/Projects/ioT/bonus/confs/values-minikube-minimum.yaml
+
+echo "wait until pods are ready"
+# kubectl -n gitlab wait $pod --for=condition=Ready --timeout=-1s
+# kubectl -n gitlab get pods -w
+
+echo "Gitlab Deployed !"
