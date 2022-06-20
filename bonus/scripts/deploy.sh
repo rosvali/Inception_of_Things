cb803f1879e8e57fa89378ff30233d699c39bba4
rosvali <rosval6578@gmail.com>
2022-06-20 11:46:53 +0200

    update deploy

--- bonus/scripts/deploy.sh
+++ bonus/scripts/deploy.sh
@@ -1,20 +1,17 @@
 echo "create namespace for gitlab"
 kubectl create namespace gitlab
 
-helm repo add gitlab https://charts.gitlab.io/
-helm repo update
+# helm repo add gitlab https://charts.gitlab.io/
+# helm repo update
 
-# git clone https://gitlab.com/gitlab-org/charts/gitlab.git
+git clone https://gitlab.com/gitlab-org/charts/gitlab.git
 cd gitlab
 
-# helm dependency update
+helm dependency update
 helm install gitlab . \
   --namespace gitlab \
   --timeout 600s \
   -f /Users/rosvali/Projects/ioT/bonus/confs/values-minikube-minimum.yaml
 
 echo "wait until pods are ready"
-# kubectl -n gitlab wait $pod --for=condition=Ready --timeout=-1s
-# kubectl -n gitlab get pods -w
-
-echo "Gitlab Deployed !"
+kubectl -n gitlab get pods
