1a33ec0af52c5070d537e1184ff79a70a4ced1a4
Roberto <r.gallego@procsea.com>
2022-08-25 15:43:29 +0200

    add -n kube-system in p2

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -8,14 +8,14 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.42.110
 
 echo "alias k='kubectl'" >> /home/vagrant/.bashrc
 
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app1/app1_deployment.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app1/app1_service.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app1/app1_ingress.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app1/app1_deployment.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app1/app1_service.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app1/app1_ingress.yaml"
 
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app2/app2_deployment.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app2/app2_service.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app2/app2_ingress.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app2/app2_deployment.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app2/app2_service.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app2/app2_ingress.yaml"
 
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app3/app3_deployment.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app3/app3_service.yaml"
-/usr/local/bin/kubectl apply -f "/vagrant/confs/app3/app3_ingress.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app3/app3_deployment.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app3/app3_service.yaml"
+/usr/local/bin/kubectl -n kube-system apply -f "/vagrant/confs/app3/app3_ingress.yaml"
